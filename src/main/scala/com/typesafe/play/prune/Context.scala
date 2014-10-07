edf7439167060f2ebfa1eca017d99327bb9d1153
Rich Dougherty <rich@rd.gen.nz>
2014-10-07 13:44:52 +1300

    Refactored command line to use subcommands

--- src/main/scala/com/typesafe/play/prune/Context.scala
+++ src/main/scala/com/typesafe/play/prune/Context.scala
@@ -3,17 +3,9 @@
  */
 package com.typesafe.play.prune
 
-import com.typesafe.config.{ Config, ConfigFactory }
-import java.io._
-import java.nio.file._
-import java.util.{ List => JList, Map => JMap, UUID }
-import java.util.concurrent.TimeUnit
-import org.apache.commons.io.{ FileUtils, IOUtils }
-import org.apache.commons.exec._
-import org.joda.time._
+import com.typesafe.config.Config
+import java.util.UUID
 import scala.collection.convert.WrapAsScala._
-import scala.concurrent._
-import scala.concurrent.duration.Duration
 
 case class Context(
   args: Args,
@@ -54,6 +46,9 @@ case class Context(
     }
   }
 
+  def playBranches = playTests.map(_.playBranch).distinct
+  def appsBranches = playTests.map(_.appsBranch).distinct
+
 //  private def getEntryNames(c: Config): Seq[String] = iterableAsScalaIterable(config.entrySet).to[Seq].foldLeft[Seq[String]](Seq.empty) {
 //    case (names, entry) => names :+ entry.getKey
 //  }
@@ -72,14 +67,64 @@ case class Context(
   }
 }
 
+sealed trait CommandArg
+case object Pull extends CommandArg
+case object Test extends CommandArg
+case object PushTestResults extends CommandArg
+case object PrintReport extends CommandArg
+case object GenerateJsonReport extends CommandArg
+
 case class Args(
+  command: Option[CommandArg] = None,
   configFile: Option[String] = None,
   dbFetch: Boolean = true,
-  dbPush: Boolean = false,
   playFetch: Boolean = true,
   appsFetch: Boolean = true,
   maxTestRuns: Option[Int] = None,
   maxWrkDuration: Option[Int] = None)
+object Args {
+  def parse(rawArgs: Seq[String]) = {
+    val parser = new scopt.OptionParser[Args]("prune") {
+      head("prune")
+      opt[String]("config-file") action { (s, c) =>
+        c.copy(configFile = Some(s))
+      }
+      cmd("pull") action { (_, c) =>
+        c.copy(command = Some(Pull))
+      } text("Pull from remote repositories") children(
+        opt[Unit]("skip-db-fetch") action { (_, c) =>
+          c.copy(dbFetch = false)
+        },
+        opt[Unit]("skip-play-fetch") action { (_, c) =>
+          c.copy(playFetch = false)
+        },
+        opt[Unit]("skip-apps-fetch") action { (_, c) =>
+          c.copy(appsFetch = false)
+        }
+      )
+      cmd("test") action { (_, c) =>
+        c.copy(command = Some(Test))
+      } text("Run tests") children(
+        opt[Int]("max-test-runs") action { (i, c) =>
+          c.copy(maxTestRuns = Some(i))
+        },
+        opt[Int]("max-wrk-duration") action { (i, c) =>
+          c.copy(maxWrkDuration = Some(i))
+        }
+      )
+      cmd("push-test-results") action { (_, c) =>
+        c.copy(command = Some(PushTestResults))
+      } text("Push test results to remote database repository")
+      cmd("print-report") action { (_, c) =>
+        c.copy(command = Some(PrintReport))
+      } text("Output a simple report of test results")
+      cmd("generate-json-report") action { (_, c) =>
+        c.copy(command = Some(GenerateJsonReport))
+      } text("Generate a report of test results to a JSON file")
+    }
+    parser.parse(rawArgs, Args()).getOrElse(sys.error("Arg parse error"))
+  }
+}
 
 case class PlayTestsConfig(
   playBranch: String,
--- src/main/scala/com/typesafe/play/prune/Prune.scala
+++ src/main/scala/com/typesafe/play/prune/Prune.scala
@@ -25,117 +25,87 @@ case class TestTaskInfo(
 object Prune {
 
   def main(rawArgs: Array[String]): Unit = {
-    val parser = new scopt.OptionParser[Args]("prune") {
-      opt[String]("config-file") action { (s, c) =>
-        c.copy(configFile = Some(s))
-      }
-      opt[Unit]("skip-fetches") action { (_, c) =>
-        c.copy(dbFetch = false, playFetch = false, appsFetch = false)
-      }
-      opt[Unit]("skip-db-fetch") action { (_, c) =>
-        c.copy(dbFetch = false)
-      }
-      opt[Unit]("db-push") action { (_, c) =>
-        c.copy(dbPush = true)
-      }
-      opt[Unit]("skip-play-fetch") action { (_, c) =>
-        c.copy(playFetch = false)
-      }
-      opt[Unit]("skip-apps-fetch") action { (_, c) =>
-        c.copy(appsFetch = false)
-      }
-      opt[Int]("max-test-runs") action { (i, c) =>
-        c.copy(maxTestRuns = Some(i))
-      }
-      opt[Int]("max-wrk-duration") action { (i, c) =>
-        c.copy(maxWrkDuration = Some(i))
-      }
-    }
-    val args = parser.parse(rawArgs, Args()).getOrElse(noReturnExit(1))
+    val args = Args.parse(rawArgs)
 
     val defaultConfig = ConfigFactory.load().getConfig("com.typesafe.play.prune")
     val userConfigFile = Paths.get(args.configFile.getOrElse(defaultConfig.getString("defaultUserConfig")))
-    if (Files.notExists(userConfigFile)) {
+
+    def configError(message: String): Unit = {
       println(s"""
-        |Please provide a Prune configuration file at $userConfigFile. You can override
-        |the configuration file location with --config-file=<path>. The file should
-        |include an instance id for this instance of Prune. An example configuration
-        |file with a new instance id is shown below. You will be able to use Prune once
-        |you've added a configuration file at $userConfigFile.
+        |$message
         |
+        |Prune is looking for a configuration file at $userConfigFile. You can override
+        |the configuration file location with --config-file=<path>.
+        |
+        |An example configuration file with a random instance id is shown below:
+        |
+        |---- prune.config ----
         |# The UUID used to identify this instance of Prune in test records.
-        |pruneInstanceId = ${UUID.randomUUID()}
+        |pruneInstanceId: ${UUID.randomUUID()}
         |
         |# The location of Java 8 on the system.
-        |#java8.home = /usr/lib/jvm/java-8-oracle/jre""".stripMargin)
+        |#java8.home: /usr/lib/jvm/java-8-oracle/jre
+        |
+        |# The location of the remote database repository to use for storing test results
+        |#dbRemote: "https://github.com/playframework/prune.git"
+        |
+        |# The branch of the database repository to use for database results
+        |#dbBranch: database""".stripMargin)
       System.exit(1)
     }
+    if (Files.notExists(userConfigFile)) configError("Please create a Prune configuration file.")
+
     val userConfig: Config = ConfigFactory.parseFile(userConfigFile.toFile)
     val config = userConfig.withFallback(defaultConfig)
 
-    if (!config.hasPath("pruneInstanceId")) {
-      println(s"""
-        |Please provide a value for "pruneInstanceId" in your configuration file at
-        |$userConfigFile. An example value with a randomly generated UUID is shown
-        |below.
-        |
-        |# The UUID used to identify this instance of Prune in test records.
-        |pruneInstanceId = ${UUID.randomUUID()}
-        |""".stripMargin)
-      System.exit(1)
+    Seq("pruneInstanceId", "java8.home", "dbRemote", "dbBranch").foreach { path =>
+      if (!config.hasPath(path)) configError(s"Missing setting `$path` from your Prune configuration file.")
     }
 
-    if (!config.hasPath("java8.home")) {
-      println(s"""
-        |Please provide a value for "java8.home" in your configuration file at
-        |$userConfigFile.
-        |
-        |# The location of Java 8 on the system.
-        |#java8.home = /usr/lib/jvm/java-8-oracle/jre
-        |""".stripMargin)
-      System.exit(1)
-    }
-
-    val ctx = Context(
+    implicit val ctx = Context(
       args = args,
       config = config
     )
 
-    main(ctx)
-  }
-
-  private def noReturnExit(code: Int): Nothing = {
-    System.exit(code)
-    throw new Exception("This code is never reached, but is used to give the method a type of Nothing")
-  }
-
-  def main(implicit ctx: Context): Unit = {
-
-    val playBranches = ctx.playTests.map(_.playBranch).distinct
-    val appsBranches = ctx.playTests.map(_.appsBranch).distinct
-
     println(s"Prune instance id is ${ctx.pruneInstanceId}")
 
-    {
-      def fetch(desc: String, switch: Boolean, remote: String, branches: Seq[String], checkedOutBranch: Option[String], localDir: String): Unit = {
-        if (switch) {
-          println(s"Fetching $desc from remote")
-          PruneGit.gitCloneOrRebaseBranches(
-            remote = remote,
-            branches = branches,
-            checkedOutBranch = checkedOutBranch,
-            localDir = localDir)
-        } else {
-          println(s"Skipping fetch of $desc from remote")
-        }
-      }
-      fetch("Prune database records", ctx.args.dbFetch, ctx.dbRemote, Seq(ctx.dbBranch), Some(ctx.dbBranch), ctx.dbHome)
-      fetch("Play source code", ctx.args.playFetch, ctx.playRemote, playBranches, None, ctx.playHome)
-      fetch("apps source code", ctx.args.appsFetch, ctx.appsRemote, appsBranches, None, ctx.appsHome)
+    args.command match {
+      case None => println("Please provide a command for Prune to execute.")
+      case Some(Pull) => pull
+      case Some(Test) => test
+      case Some(PushTestResults) => pushTestResults
+      case Some(PrintReport) => printReport
+      case Some(GenerateJsonReport) => ???
     }
 
-    def playCommitsToTest(playTestConfig: PlayTestsConfig): Seq[String] =
-      PruneGit.gitFirstParentsLog(ctx.playHome, playTestConfig.playBranch, playTestConfig.playRevisionRange._1, playTestConfig.playRevisionRange._2)
+  }
+
+  def pull(implicit ctx: Context): Unit = {
+    def pull0(desc: String, switch: Boolean, remote: String, branches: Seq[String], checkedOutBranch: Option[String], localDir: String): Unit = {
+      if (switch) {
+        println(s"Fetching $desc from remote")
+        PruneGit.gitCloneOrRebaseBranches(
+          remote = remote,
+          branches = branches,
+          checkedOutBranch = checkedOutBranch,
+          localDir = localDir)
+      } else {
+        println(s"Skipping fetch of $desc from remote")
+      }
+    }
+    pull0("Prune database records", ctx.args.dbFetch, ctx.dbRemote, Seq(ctx.dbBranch), Some(ctx.dbBranch), ctx.dbHome)
+    pull0("Play source code", ctx.args.playFetch, ctx.playRemote, ctx.playBranches, None, ctx.playHome)
+    pull0("apps source code", ctx.args.appsFetch, ctx.appsRemote, ctx.appsBranches, None, ctx.appsHome)
+  }
+
+  private def playCommitsToTest(playTestConfig: PlayTestsConfig)(implicit ctx: Context): Seq[String] =
+    PruneGit.gitFirstParentsLog(
+      ctx.playHome,
+      playTestConfig.playBranch,
+      playTestConfig.playRevisionRange._1,
+      playTestConfig.playRevisionRange._2)
+
+  def test(implicit ctx: Context): Unit = {
 
     val neededTasks: Seq[TestTask] = ctx.playTests.flatMap { playTest =>
       //println(s"Working out tests to run for $playTest")
@@ -190,61 +160,59 @@ object Prune {
     Assets.extractAssets
 
     truncatedTasksToRun.foreach(RunTest.runTestTask)
+  }
 
-    {
-      type PlayRev = String
-      case class TestResult(
-        testRunId: UUID,
-        wrkOutput: Option[String]
-      )
+  def printReport(implicit ctx: Context): Unit = {
+    type PlayRev = String
+    case class TestResult(
+                           testRunId: UUID,
+                           wrkOutput: Option[String]
+                           )
 
-      def getResults(playCommits: Seq[PlayRev], testName: String): Map[PlayRev, TestResult] = {
-        DB.foldLeft[Map[PlayRev,TestResult]](Map.empty) {
-          case (results, join) =>
-            if (
-              join.pruneInstanceId == ctx.pruneInstanceId &&
+    def getResults(playCommits: Seq[PlayRev], testName: String): Map[PlayRev, TestResult] = {
+      DB.foldLeft[Map[PlayRev,TestResult]](Map.empty) {
+        case (results, join) =>
+          if (
+            join.pruneInstanceId == ctx.pruneInstanceId &&
               join.testRunRecord.testName == testName &&
               playCommits.contains(join.playBuildRecord.playCommit)) {
-              results.updated(join.playBuildRecord.playCommit, TestResult(
-                testRunId = join.testRunId,
-                wrkOutput = join.testRunRecord.wrkExecutions.last.stdout
-              ))
-            } else results
-        }
+            results.updated(join.playBuildRecord.playCommit, TestResult(
+              testRunId = join.testRunId,
+              wrkOutput = join.testRunRecord.wrkExecutions.last.stdout
+            ))
+          } else results
       }
+    }
 
-      for {
-        playTestConfig <- ctx.playTests
-        testName <- playTestConfig.testNames
-      } {
-        val playCommits = playCommitsToTest(playTestConfig)
-        val resultMap = getResults(playCommits, testName)
-        println(s"Test $testName on ${playTestConfig.playBranch}")
-        for (playCommit <- playCommits) {
-          val wrkOutput: Option[String] = resultMap.get(playCommit).flatMap(_.wrkOutput)
-          val wrkResult: Option[WrkResult] = wrkOutput.flatMap(Results.parseWrkOutput)
-          val resultDisplay: String = wrkResult.map { wr =>
-            s"Requests/s: ${wr.requests.toDouble / wr.duration.toDouble * 1000000}, "+
-            s"Latency 50%: ${wr.latency.percentiles(50)}, " +
+    for {
+      playTestConfig <- ctx.playTests
+      testName <- playTestConfig.testNames
+    } {
+      val playCommits: Seq[String] = playCommitsToTest(playTestConfig)
+      val resultMap = getResults(playCommits, testName)
+      println(s"Test $testName on ${playTestConfig.playBranch}")
+      for (playCommit <- playCommits) {
+        val wrkOutput: Option[String] = resultMap.get(playCommit).flatMap(_.wrkOutput)
+        val wrkResult: Option[WrkResult] = wrkOutput.flatMap(Results.parseWrkOutput)
+        val resultDisplay: String = wrkResult.map { wr =>
+          s"Requests/s: ${wr.requests.toDouble / wr.duration.toDouble * 1000000}, "+
+            s"Mean latency: ${wr.latency.mean}, " +
             s"Latency 95%: ${wr.latency.percentiles(95)}"
-          }.getOrElse("-")
-          println(s"${playCommit.substring(0,7)} $resultDisplay")
-        }
+        }.getOrElse("-")
+        println(s"${playCommit.substring(0,7)} $resultDisplay")
       }
     }
+  }
 
-    // Push database results
-    {
-      if (ctx.args.dbPush) {
-        PruneGit.gitPushChanges(
-          remote = ctx.dbRemote,
-          branch = ctx.dbBranch,
-          localDir = ctx.dbHome)
-      } else {
-        println(s"Not pushing results database: use --db-push argument to push results")
-      }
-    }
+  def generateJsonReport(implicit ctx: Context): Unit = {
+    ???
+  }
 
+  def pushTestResults(implicit ctx: Context): Unit = {
+    PruneGit.gitPushChanges(
+      remote = ctx.dbRemote,
+      branch = ctx.dbBranch,
+      localDir = ctx.dbHome)
   }
 
 }
