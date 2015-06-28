baeb5c573788abfc41701ab5e0c51319462d1c20
Rich Dougherty <rich@rd.gen.nz>
2015-06-29 09:06:22 +1200

    Made version detection regex more lenient

--- src/main/scala/com/typesafe/play/prune/PlayVersion.scala
+++ src/main/scala/com/typesafe/play/prune/PlayVersion.scala
@@ -9,14 +9,17 @@ import java.util.regex._
 object PlayVersion {
 
   def readPlayVersion()(implicit ctx: Context): String = {
+    val versionFilePath = Paths.get(ctx.playHome, "framework/version.sbt")
     val fileString: String = {
-      val versionFilePath = Paths.get(ctx.playHome, "framework/version.sbt")
       val fileBytes = Files.readAllBytes(versionFilePath)
       new String(fileBytes, "UTF-8").trim
     }
-    val pattern = Pattern.compile("Release.branchVersion in ThisBuild := \"(.*)\"")
+    // Examples to match:
+    // - Release.branchVersion in ThisBuild := "2.4-SNAPSHOT"
+    // - version in ThisBuild := "2.5.0-SNAPSHOT"
+    val pattern = Pattern.compile("ersion in ThisBuild := \"(.*)\"")
     val matcher = pattern.matcher(fileString)
-    assert(matcher.matches())
+    assert(matcher.matches(), s"Couldn't read Play version from $versionFilePath: regex didn't match")
     val version = matcher.group(1)
     version
   }
