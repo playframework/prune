03033a4796b059f07cff1fcc9be2c2a3b23fb717
James Roper <james@jazzy.id.au>
2015-08-20 15:21:51 +1000

    Switch to using Akka streams

--- scala-di-bench/app/controllers/Application.scala
+++ scala-di-bench/app/controllers/Application.scala
@@ -1,44 +1,39 @@
 package controllers
 
 import javax.inject.{Inject, Singleton}
-import play.api._
 import play.api.mvc._
-import play.api.libs.iteratee.Enumerator
 import play.api.libs.json.Json
-import scala.concurrent.{ExecutionContext, Future}
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
 
 @Singleton
-class Application @Inject() (implicit
-  ec: ExecutionContext
-) extends Controller {
+class Application extends Controller {
 
   def simple = Action { request =>
     Ok("Hello world.")
   }
 
   def download(length: Int) = Action { request =>
-    Ok(new Array[Byte](length))
+    Ok(ByteString(new Array[Byte](length)))
   }
 
   def downloadChunked(length: Int) = Action { request =>
 
-    @volatile
-    var remaining = length
     val maxArraySize = 4 * 1024
+    val numFullChunks = length / maxArraySize
+    val lastChunkSize = length % maxArraySize
 
-    val arrayEnum = Enumerator.generateM {
-      val arraySize = Math.min(remaining, maxArraySize)
-      val optArray = if (arraySize == 0) None else {
-        remaining -= arraySize
-        Some(new Array[Byte](arraySize))
-      }
-      Future.successful(optArray)
+    val fullChunks = Source(1 to numFullChunks).map { _ =>
+      ByteString(new Array[Byte](maxArraySize))
     }
 
-    Result(
-      header = ResponseHeader(200),
-      body = arrayEnum
-    )
+    val allChunks = if (lastChunkSize > 0) {
+      fullChunks ++ Source.single(ByteString(new Array[Byte](lastChunkSize)))
+    } else {
+      fullChunks
+    }
+
+    Ok.chunked(allChunks)
   }
 
   def upload = Action(parse.raw) { request =>
