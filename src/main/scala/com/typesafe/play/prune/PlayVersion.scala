aa12d1c60536d572b10bd2b334361c45335b970b
Rich Dougherty <rich@rd.gen.nz>
2016-09-19 10:20:27 +1200

    Parse version.sbt when it includes comments

--- src/main/scala/com/typesafe/play/prune/PlayVersion.scala
+++ src/main/scala/com/typesafe/play/prune/PlayVersion.scala
@@ -24,7 +24,12 @@ object PlayVersion {
     // Examples to match:
     // - Release.branchVersion in ThisBuild := "2.4-SNAPSHOT"
     // - version in ThisBuild := "2.5.0-SNAPSHOT"
-    val pattern = Pattern.compile("(.*)ersion in ThisBuild := \"(.*)\"")
+    // - //
+    //   // Copyright (C) 2009-2016 Typesafe Inc. <http://www.typesafe.com>
+    //   //
+    //
+    //   version in ThisBuild := "2.5.0-SNAPSHOT"
+    val pattern = Pattern.compile("(.*)ersion in ThisBuild := \"(.*)\"(.*)", Pattern.DOTALL)
     val matcher = pattern.matcher(cleanedString)
     assert(matcher.matches(), s"Couldn't read Play version from [$cleanedString]: regex didn't match")
     val version = matcher.group(2)
